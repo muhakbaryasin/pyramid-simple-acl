f9da432c46ff25afa86e4708ca328fd7635e2c42
akbar.yasin <akbar.yasin@versatech.co.id>
2017-12-05 11:58:03 +0700

    Wont authenticate
    Signed-off-by: akbar.yasin <akbar.yasin@versatech.co.id>

--- pyramid_simple_acl/models/mymodel.py
+++ pyramid_simple_acl/models/mymodel.py
@@ -3,11 +3,94 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    String,
+    Date,
+    ForeignKey,
 )
 
 from .meta import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+class TblRole(Base):
+    __tablename__ = "tbl_role"
+    role_id = Column(Integer, primary_key=True, autoincrement=True)
+    role_name = Column(String(50), nullable=False)
+    role_order = Column(Integer, nullable=False)
+    role_description = Column(Text, nullable=False)
+
+    def __init__(self, role_name, role_order, role_description):
+        self.role_name = role_name
+        self.role_order = role_order
+        self.role_description = role_description
 
 
+class TblRoom(Base):
+    __tablename__ = "tbl_room"
+    room_number = Column(Integer, primary_key=True, nullable=False)
+    room_type = Column(String(6), nullable=False)
+    room_floor = Column(Integer, nullable=False)
+    room_is_reserved = Column(Integer, nullable=False)
+
+    def __init__(self, room_number, room_type, room_floor, room_is_reserved):
+        self.room_number = room_number
+        self.room_type = room_type
+        self.room_floor = room_floor
+        self.room_is_reserved = room_is_reserved
+
+class TblUser(Base):
+    __tablename__ = "tbl_user"
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String(50), nullable=False)
+    user_password = Column(String(128), nullable=False)
+    user_is_login = Column(Integer, nullable=False)
+    # role_id = Column(Integer, nullable=False)
+    role_id = Column(Integer, ForeignKey('tbl_role.role_id'))
+    role = relationship('TblRole', backref='userrole')
+
+    def __init__(self, user_name, user_password, user_is_login, role):
+        self.user_name = user_name
+        self.user_password = user_password
+        self.user_is_login = user_is_login
+        self.role = role
+
+class TblReservation(Base):
+    __tablename__ = "tbl_reservation"
+    reserv_id = Column(Integer, primary_key=True, autoincrement=True)
+    # room_number = Column(Integer, nullable=False)
+    reserve_start_date = Column(Date, default=func.timezone('UTC', func.current_timestamp()), nullable=False)
+    reserve_end_date = Column(Date, nullable=False)
+    # cust_id = Column(Integer, nullable=False)
+    room = relationship('TblRoom', backref='reserveroom')
+    room_number = Column(Integer, ForeignKey('tbl_room.room_number'))
+    
+    cust_id = Column(Integer, ForeignKey('tbl_customer.cust_id'))
+    cust = relationship('TblCustomer', backref='reservecust')
+
+    def __init__(self, reserve_start_date, reserve_end_date, room, cust):
+        self.reserve_start_date = reserve_start_date
+        self.reserve_end_date = reserve_end_date
+        self.room = room
+        self.cust = cust
+
+    
+class TblCustomer(Base):
+    __tablename__ = "tbl_customer"
+    cust_id = Column(Integer, primary_key=True, autoincrement=True)
+    cust_first_name = Column(String(50), nullable=False)
+    cust_last_name = Column(String(50), nullable=False)
+    cust_address = Column(Text, nullable=False)
+    cust_phone = Column(String(25), nullable=False)
+    cust_profile = Column(String(4), nullable=False)
+
+    def __init__(self, cust_first_name, cust_last_name, cust_address, cust_phone, cust_profile):
+        self.cust_first_name = first_name
+        self.cust_last_name = cust_last_name
+        self.cust_address = cust_address
+        self.cust_phone = cust_phone
+        self.cust_profile = cust_profile
+
+"""
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
@@ -16,3 +99,4 @@ class MyModel(Base):
 
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+"""
--- pyramid_simple_acl/views/default.py
+++ pyramid_simple_acl/views/default.py
@@ -1,20 +1,77 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
+from pyramid.security import remember, forget, authenticated_userid
+
 from sqlalchemy.exc import DBAPIError
 
-from ..models import MyModel
+from ..models import TblUser, TblReservation, TblRoom
+from pyramid.httpexceptions import (
+    HTTPFound,
+    )
 
+import logging
+log = logging.getLogger(__name__)
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
     try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
+        pass
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'pyramid-simple-acl'}
+    return {'one': 'one', 'project': 'pyramid-simple-acl'}
 
+@view_config(route_name='login', renderer='../templates/login.jinja2')
+def login(request):
+    try:
+        if not ('user_name' in request.params and 'password' in request.params):
+            return {}
+        
+        if request.params['user_name'] == '' or request.params['password'] == '':
+            raise Exception('Ada inputan yang kosong dari form')
+
+        match_ = request.dbsession.query(TblUser).filter_by(user_name=request.params['user_name'], user_password=request.params['password']).one()
+        username = request.params['user_name']
+        
+        if match_ is not None:
+            headers = remember(request, username)
+            request.response.headerlist.extend(headers)
+            next_url = request.route_url('search-room')
+            
+            return HTTPFound(location=next_url)
+        
+    except Exception as e:
+        log.exception(str(e))
+        return {'code' : 'error', 'message' : str(e) }
+    
+@view_config(route_name='logout', renderer='../templates/logout.jinja2')
+def logout(request):
+        headers = forget(request)
+        request.response.headerlist.extend(headers)
+        next_url = request.route_url('login')
+            
+        return HTTPFound(location=next_url)
+
+@view_config(route_name='search-room', renderer='../templates/search-room.jinja2', permission='edit')
+def search_room(request):
+    try:
+        if not ('floor' in request.params):
+            return {}
+        
+        if request.params['floor'] == '':
+            raise Exception('Ada inputan yang kosong dari form')
+        
+        query = request.dbsession.query(TblReservation)
+        result = query.join(TblRoom, aliased=True).filter_by(room_floor=request.params['floor']).all()
+        
+        if result is None or len(result) < 1:
+            raise Exception("No row found")
+        
+        return {'code' : 'ok', 'message' : '', 'content' : result }
+        
+    except Exception as e:
+        log.exception(str(e))
+        return {'code' : 'error', 'message' : str(e), 'content' : ''}
 
 db_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
